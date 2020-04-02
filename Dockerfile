62448f045925cdb116fd09c9b4bf2dbfeb1a93c7
Tim Brand <tim.brand@app-artment.com>
2020-04-02 12:43:39 +0200

    WIP

--- Dockerfile
+++ Dockerfile
@@ -5,3 +5,4 @@ VOLUME [ "/var/run/docker.sock", "/var/run/docker.sock" ]
 ADD scripts/mic-rotate-auth.sh /
 ADD scripts/project-build.sh /
 ADD scripts/test-integration.sh /
+ADD scripts/docker-compose.yml /
--- scripts/test-integration.sh
+++ scripts/test-integration.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
 echo "Starting integration test..."
+
+echo "Currently running containers:"
 docker ps -a
+docker inspect ${docker ps -q}
+
+# echo "Starting docker-compose configuration"
+# docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -w $PWD docker/compose -f /docker-compose.yml up --abort-on-container-exit
+
 echo "Integration test finished!"
