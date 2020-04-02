2d38b74d1cf8173f6d0bfe6288c4792c54007646
Tim Brand <tim.brand@app-artment.com>
2020-04-02 12:46:51 +0200

    typo

--- scripts/test-integration.sh
+++ scripts/test-integration.sh
@@ -4,7 +4,7 @@ echo "Starting integration test..."
 
 echo "Currently running containers:"
 docker ps -a
-docker inspect ${docker ps -q}
+docker inspect $(docker ps -q)
 
 # echo "Starting docker-compose configuration"
 # docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -w $PWD docker/compose -f /docker-compose.yml up --abort-on-container-exit
