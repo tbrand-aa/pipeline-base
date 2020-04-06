967419522f07180e13dbbd5cdf9db5dd337f9831
Tim Brand <tim.brand@app-artment.com>
2020-04-06 13:44:07 +0200

    Escaping

--- scripts/update-gh-commit-status.sh
+++ scripts/update-gh-commit-status.sh
@@ -3,7 +3,7 @@
 URL="https://api.github.com/repos/${WORK_REPOSITORY_NAME}/statuses/${WORK_REPOSITORY_SHA}"
 echo "Update URL: ${URL}"
 
-UPDATE_COMMAND="curl -XPOST -s -o /dev/null -w '%{http_code}' -H 'Authorization: token ${PA_TOKEN}' -H 'Accept: application/vnd.github.everest-preview+json' -H 'Content-Type: application/json' ${URL} --data '{"state": "success"}'"
+UPDATE_COMMAND="curl -XPOST -s -o /dev/null -w '%{http_code}' -H 'Authorization: token ${PA_TOKEN}' -H 'Accept: application/vnd.github.everest-preview+json' -H 'Content-Type: application/json' ${URL} --data '{\"state\": \"success\"}'"
 echo "Update command: ${UPDATE_COMMAND}"
 
 STATUS_CODE=$($COMMAND)
