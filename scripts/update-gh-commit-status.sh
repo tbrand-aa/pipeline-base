ef32910f4515805e089d7ff3d09264d74d983d79
Tim Brand <tim.brand@app-artment.com>
2020-04-06 14:30:46 +0200

    refactor fix

--- scripts/update-gh-commit-status.sh
+++ scripts/update-gh-commit-status.sh
@@ -4,15 +4,15 @@ URL="https://api.github.com/repos/${WORK_REPOSITORY_NAME}/statuses/${WORK_REPOSI
 echo "Commit status update URL: ${URL}"
 
 STATUS_CODE=$(
-  curl 
-  -XPOST 
-  -s 
-  -o /dev/null 
-  -w '%{http_code}' 
-  -H "Authorization: token ${PA_TOKEN}" 
-  -H 'Accept: application/vnd.github.everest-preview+json' 
-  -H 'Content-Type: application/json' 
-  ${URL} 
+  curl \
+  -XPOST \
+  -s \
+  -o /dev/null \
+  -w '%{http_code}' \
+  -H "Authorization: token ${PA_TOKEN}" \
+  -H 'Accept: application/vnd.github.everest-preview+json' \
+  -H 'Content-Type: application/json' \
+  ${URL} \
   --data '{"state": "success"}'
 )
 if [ $STATUS_CODE -eq 201 ]
