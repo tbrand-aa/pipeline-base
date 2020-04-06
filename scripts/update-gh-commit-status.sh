e5479d2c66f5feafceaa3722be3e6e46b63e0aa8
Tim Brand <tim.brand@app-artment.com>
2020-04-06 13:39:15 +0200

    Debugging script

--- scripts/update-gh-commit-status.sh
+++ scripts/update-gh-commit-status.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
 URL="https://api.github.com/repos/${WORK_REPOSITORY_NAME}/statuses/${WORK_REPOSITORY_SHA}"
+echo "Update URL: ${URL}"
 
-echo "Calling endpoint: ${URL}"
+UPDATE_COMMAND="curl -XPOST -s -o /dev/null -w '%{http_code}' -H 'Authorization: token ${PA_TOKEN}' -H 'Accept: application/vnd.github.everest-preview+json' -H 'Content-Type: application/json' ${URL} --data '{"state": "success"}'"
+echo "Update command: ${UPDATE_COMMAND}"
 
-STATUS_CODE=$(curl -XPOST -s -o /dev/null -w '%{http_code}' -H "Authorization: token ${PA_TOKEN}" -H "Accept: application/vnd.github.everest-preview+json" -H "Content-Type: application/json" ${URL} --data '{"state": "success"}')
+STATUS_CODE=$($COMMAND)
 if [ $STATUS_CODE -eq 200 ]
 then
   echo "  Request succesful!"
