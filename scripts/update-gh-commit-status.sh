f6b923bef4d34282c667d19a64ae31baafdaf627
Tim Brand <tim.brand@app-artment.com>
2020-04-06 13:17:39 +0200

    Added script for commit status update

---
+++ scripts/update-gh-commit-status.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+URL="https://api.github.com/repos/${WORK_REPOSITORY_NAME}/statuses/${WORK_REPOSITORY_SHA}"
+
+echo "Calling endpoint: ${URL}"
+
+STATUS_CODE=$(curl -XPOST -s -o /dev/null -w '%{http_code}' -H "Authorization: token ${PA_TOKEN}" -H "Accept: application/vnd.github.everest-preview+json" -H "Content-Type: application/json" ${URL} --data '{"state": "success"}')
+if [ $STATUS_CODE -eq 200 ]
+then
+  echo "  Request succesful!"
+  exit 0
+else 
+  echo "  Request failed with StatusCode: ${STATUS_CODE}"
+  exit 1
+fi
+
