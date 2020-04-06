9c53a08e8e06df9e7b81e4ddee5bbe8c020da120
Tim Brand <tim.brand@app-artment.com>
2020-04-06 12:41:26 +0200

    tryout for exitcode

--- scripts/test-integration.sh
+++ scripts/test-integration.sh
@@ -10,7 +10,7 @@ echo "Starting integration test..."
 # docker inspect $(docker ps -q)
 
 echo "Starting docker-compose configuration"
-docker run --rm -v $PWD/work:$PWD/work -v /var/run/docker.sock:/var/run/docker.sock -w $PWD/work docker/compose -f docker-compose.yml up --abort-on-container-exit
+docker run --rm -v $PWD/work:$PWD/work -v /var/run/docker.sock:/var/run/docker.sock -w $PWD/work docker/compose -f docker-compose.yml up --abort-on-container-exit --exit-code-from node
 echo "Removing docker-compose configuration resources"
 docker run --rm -v $PWD/work:$PWD/work -v /var/run/docker.sock:/var/run/docker.sock -w $PWD/work docker/compose -f docker-compose.yml rm -fsv
 
